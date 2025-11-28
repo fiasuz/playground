63f1bd2d408f69efb20129b173aec9d3613f9003
azizziy <usmonovazizbek045@gmail.com>
2025-11-28 20:22:22 +0500

    refactor: packages ui

--- apps/dashboard/src/App.tsx
+++ apps/dashboard/src/App.tsx
@@ -1,17 +1,8 @@
-import { Button } from "@repo/ui/button";
-import { Card } from "@repo/ui/card";
-import { Input } from "@repo/ui/input";
-
 function App() {
   return (
-    <>
-      <Button>sdasdsaa</Button>
-      <div className="h-10 bg-red-500 w-10"></div>
-      <Card title="title" href="https://example.com">
-        asdasd
-      </Card>
-      <Input />
-    </>
+    <div>
+      <h1>dashboard</h1>
+    </div>
   );
 }
 
